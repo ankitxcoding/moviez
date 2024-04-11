e20f3afbae4b441242ac79dcf989de5f05075f98
ankitxcoding <ankitxcoding@gmail.com>
2024-04-11 23:33:48 +0530

    homepage movie cards completed with name and rating

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,32 @@
 import React from "react";
-import { Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Image, Text, TouchableOpacity, StyleSheet, View } from "react-native";
 import { BASE_POSTER_URL } from "../utils/constants";
 import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const MovieCard = (props) => {
-  const { moviesList } = props;
+const MovieCard = ({ moviesList }) => {
   const { title, poster_path, vote_average } = moviesList;
   const navigation = useNavigation();
 
   const handleMoviePress = () => {
     navigation.navigate("Details", { movie: moviesList });
+  };
+
+  const renderStars = () => {
+    const rating = vote_average / 2;
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<Icon key={i} name="star" size={20} color="#FFD700" />);
+      } else if (i - rating <= 0.5) {
+        stars.push(
+          <Icon key={i} name="star-half-o" size={20} color="#FFD700" />
+        );
+      } else {
+        stars.push(<Icon key={i} name="star-o" size={20} color="#FFD700" />);
+      }
+    }
+    return stars;
   };
 
   return (
@@ -18,14 +35,13 @@ const MovieCard = (props) => {
         style={styles.image}
         source={{ uri: BASE_POSTER_URL + poster_path }}
       />
-      <Text style={styles.text}>{title}</Text>
-      <Text style={styles.vote}>
-        {vote_average === 0 ? (
-          <span className="text-white">NR</span>
-        ) : (
-          vote_average && vote_average.toFixed(1)
-        )}
-      </Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{title}</Text>
+        <View style={styles.voteContainer}>
+          {renderStars()}
+          <Text style={styles.vote}>{vote_average.toFixed(1)}</Text>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -34,27 +50,31 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
-    flexGrow: 1,
   },
   image: {
     width: 165,
     aspectRatio: 2 / 3,
   },
-  text: {
-    color: "#fff",
+  textContainer: {
+    backgroundColor: "#E50914",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    backgroundColor: "#E50914",
-    flex: 1,
     alignSelf: "stretch",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  voteContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
   },
   vote: {
     color: "#fff",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    backgroundColor: "#E50914",
-    flex: 1,
-    alignSelf: "stretch",
+    marginLeft: 5,
+    fontWeight: "bold",
   },
 });
 export default MovieCard;
