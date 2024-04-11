dfcbcf204d4b461a9dded7fded6bcbe110b53829
ankitxcoding <ankitxcoding@gmail.com>
2024-04-11 23:18:38 +0530

    fixed few errors

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, View, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
 import MovieDetails from "./src/components/MovieDetails";
