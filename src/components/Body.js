2e2e8061f1567b9c7f6195961c68084e757c52de
ankitxcoding <ankitxcoding@gmail.com>
2024-04-20 17:46:47 +0530

    fixed movie card container

--- src/components/Body.js
+++ src/components/Body.js
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
   movieContainer: {
     width: "45%",
     marginBottom: 20,
+    alignItems: "center"
   },
   loaderContainer: {
     alignItems: "center",
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Text, TouchableOpacity, StyleSheet, View } from "react-native";
+import { Image, Text, Pressable, StyleSheet, View } from "react-native";
 import { BASE_POSTER_URL } from "../utils/constants";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -30,39 +30,42 @@ const MovieCard = ({ moviesList }) => {
   };
 
   return (
-    <TouchableOpacity style={styles.container} onPress={handleMoviePress}>
-      <Image
-        style={styles.image}
-        source={{ uri: BASE_POSTER_URL + poster_path }}
-      />
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>{title}</Text>
-        <View style={styles.voteContainer}>
-          {renderStars()}
-          <Text style={styles.vote}>{vote_average.toFixed(1)}</Text>
+    <View style={styles.container}>
+      <Pressable onPress={handleMoviePress}>
+        <Image
+          style={styles.image}
+          source={{ uri: BASE_POSTER_URL + poster_path }}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{title}</Text>
+          <View style={styles.voteContainer}>
+            {renderStars()}
+            <Text style={styles.vote}>{vote_average.toFixed(1)}</Text>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "#E50914",
+    width: "100%",
+    borderRadius: 5,
+    overflow: "hidden",
   },
   image: {
-    width: 165,
+    width: "100%",
     aspectRatio: 2 / 3,
+    resizeMode: 'cover',
   },
   textContainer: {
     flex: 1,
-    justifyContent: "space-evenly",
-    backgroundColor: "#E50914",
     paddingHorizontal: 10,
     paddingVertical: 5,
     alignSelf: "stretch",
-    minHeight: 100,
   },
   text: {
     color: "#fff",
